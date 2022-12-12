a0f6dcc07ba278adcd71a71ee8f528496c640ad6
Dianna Sharmazanyan <116112717+diannaSharmazanyan-qa@users.noreply.github.com>
2022-12-12 16:27:23 +0300

    Update test_pokemon.py

--- PythonProjects/TESTS/test_pokemon.py
+++ PythonProjects/TESTS/test_pokemon.py
@@ -6,5 +6,5 @@ def test_status_code():
     assert response.status_code == 200
 
 def test_string_in_body():
-    response = requests.get ('https://pokemonbattle.me:5000/trainers', params={'trainer_id': '1293'})
-    assert response.json()['trainer_name'] == 'Dean Block'
+    response = requests.get ('https://pokemonbattle.me:5000/trainers', params={'trainer_id': 'id_тренера'})
+    assert response.json()['trainer_name'] == 'id_тренера'
